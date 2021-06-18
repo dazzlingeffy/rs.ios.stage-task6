7da7287cff1f8c8a5830b8d9fd930113127650ff
dazzlingeffy <irusya9@mail.ru>
2021-06-18 19:42:43 +0300

    feat: CoronaClass ✅

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -2,17 +2,41 @@ import Foundation
 
 class CoronaClass {
  
-     var seats = [Int]()
+    var seats = [Int]()
+    let n: Int
 
      init(n: Int) {
- 
+        self.n = n
      }
      
      func seat() -> Int {
-        -1
+        var res = 0
+        var diff = 0
+        
+        if !seats.contains(n - 1) {
+            res = n - 1
+        }
+        if seats.contains(0) {
+        
+            var i = 0;
+            while i < seats.count - 1 {
+                let tmp = abs(seats[i + 1] - seats[i]) / 2
+                if tmp > diff {
+                    diff = tmp
+                    res = seats[i] + tmp
+                }
+                i += 1
+            }
+        }
+        
+        seats.append(res)
+        seats.sort()
+        return res
      }
      
      func leave(_ p: Int) {
-         
+        if seats.contains(p) {
+            seats.remove(at: seats.firstIndex(of: p)!)
+        }
      }
 }
